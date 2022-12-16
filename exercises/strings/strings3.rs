ad46742931097f903cb8f61c464bf3b6c77103df
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2022-12-16 11:01:25 +0100

    .

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -5,12 +5,12 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.to_string().trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.push_str(" world!")
 }
 
 fn replace_me(input: &str) -> String {
